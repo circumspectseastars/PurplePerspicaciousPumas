d2a31125723138915d7aa01fa560f5a103eef5ce
kevinhuangg <email.kevinh@gmail.com>
2017-03-30 13:46:21 -0700

    added tab button component, create interactive button to switch between game and achievement tabs, connect server and frontend so when userprofile is rendered info can be retrieved from db

--- server/index.js
+++ server/index.js
@@ -116,6 +116,10 @@ app.get('/logout', function(req, res) {
   });
 })
 
+app.get('/userprofile', function(req, res) {
+  console.log('get request from user profile received')
+  res.status(200).send({});
+});
 
 //don't need the app.options
 var server = app.listen(port, function() {
